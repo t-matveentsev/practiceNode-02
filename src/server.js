73618c3e45aa9f80f415116aaa00caffd51d86ec
Artem Matvieientsev <tmatveentsev@gmail.com>
2025-03-30 21:55:32 +0200

    start file and server

---
+++ src/server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import cors from "cors";
+import pino from "pino-http";
+import "dotenv/config";
+
+export const startServer = () => {
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
+  app.use(
+    pino({
+      transport: {
+        target: "pino-pretty",
+      },
+    })
+  );
+
+  app.get("/", (req, res) => {
+    res.json({
+      message: "Server start successfully",
+    });
+  });
+
+  app.use((req, res) => {
+    res.status(404).json({
+      message: `${req.url} not found`,
+    });
+  });
+
+  app.use((error, req, res, next) => {
+    res.status(500).json({
+      message: error.message,
+    });
+  });
+
+  const port = Number(process.env.PORT);
+
+  app.listen(port, () => console.log(`Server running on ${port} port`));
+};
