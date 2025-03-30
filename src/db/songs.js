96c8e5174797fb9733b99bbc4c3ff30f7df06886
Artem Matvieientsev <tmatveentsev@gmail.com>
2025-03-30 18:56:26 +0200

    example 2

---
+++ src/db/songs.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: "807f51cb-6884-40ae-a84f-4be8616996b0",
+    album: "Let's Talk About Love",
+    artist: "John Fred & The Playboy Band",
+    genre: "Urban",
+    songName: "Eight Days a Week",
+  },
+  {
+    id: "9ee6baee-7f3f-4c0c-be92-4ede3ad638d6",
+    album: "No Angel",
+    artist: "gnash",
+    genre: "Latin",
+    songName: "Save the Last Dance For Me",
+  },
+  {
+    id: "3cdf8895-c2c6-40ee-be9c-b3024fb96ece",
+    album: "The Golden Child",
+    artist: "Cheat Codes",
+    genre: "Anime",
+    songName: "Brown Sugar",
+  },
+  {
+    id: "db50e10b-631c-48e7-8fed-ae0525e2ccbd",
+    album: "Current Mood",
+    artist: "U2",
+    genre: "CCM",
+    songName: "Sunday, Monday or Always",
+  },
+  {
+    id: "ce009303-0e29-4f72-a9e3-dd2ea834e48d",
+    album: "Rock N Roll Jesus",
+    artist: "Dire Straits",
+    genre: "Blue Note",
+    songName: "The Hustle",
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import express from "express";
+import songs from "./db/songs.js";
 
 const app = express(); // app - web server
+
+app.set("json spaces", 8);
 
 app.get("/", (request, response) => {
   response.send("<h1>Home page</h1>");
@@ -10,6 +13,11 @@ app.get("/contacts", (request, response) => {
   console.log(request.method);
   console.log(request.url);
   response.send("<h1>Contacts Page</h1>");
+});
+
+app.get("/songs", (req, res) => {
+  res.json(songs);
+  // res.send(songs);
 });
 
 app.listen(3000, () => console.log("server running on 3000 port"));
