26fb11f1a53002edb6d685444810032ddf5a88ff
Artem Matvieientsev <tmatveentsev@gmail.com>
2025-04-02 16:11:16 +0200

    mognoDB, base kod

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import cors from "cors";
 import pino from "pino-http";
-import "dotenv/config";
+
+import { getMovies, getMovieById } from "./services/movies.js";
+
+import { getEnvVar } from "./utils/getEnvVar.js";
 
 export const startServer = () => {
   const app = express();
@@ -15,9 +18,23 @@ export const startServer = () => {
     })
   );
 
-  app.get("/", (req, res) => {
+  app.get("/api/movies", async (req, res) => {
+    const data = await getMovies();
     res.json({
-      message: "Server start successfully",
+      status: 200,
+      message: "Successfully find movies",
+      data,
+    });
+  });
+
+  app.get("/api/movies/:id", async (req, res) => {
+    const { id } = req.params;
+    const data = await getMovieById(id);
+
+    res.json({
+      status: 200,
+      message: `Successfully find movie with id=${id}`,
+      data,
     });
   });
 
@@ -33,7 +50,7 @@ export const startServer = () => {
     });
   });
 
-  const port = Number(process.env.PORT);
+  const port = Number(getEnvVar("PORT", 3000));
 
   app.listen(port, () => console.log(`Server running on ${port} port`));
 };
